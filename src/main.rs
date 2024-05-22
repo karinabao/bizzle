9f2d79f3e60b91c230756054588dc99f69820117
Karina Bao <karinabao@Karinas-MacBook-Pro.local>
2024-05-22 17:36:40 -0400

    v2 update

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,21 @@ use rocket::{get, routes, launch, Build, Rocket, form::{Form, FromForm}, State};
 use rocket::serde::{Deserialize, Serialize, json::Json};
 use rand::seq::SliceRandom;
 use std::{sync::RwLock, fs::File};
+use num_format::{Locale, ToFormattedString};
 
 #[macro_use] extern crate rocket;
 
 #[derive(Debug, Deserialize, Serialize, Clone)]
 struct Company {
     name: String,
+    ticker: String,
     rank: u32,
+    year: u32,
     industry: String,
     sector: String,
     headquarters_state: String,
     headquarters_city: String,
-    market_value_mil: u64,
+    marketcap: u64,
     revenue_mil: u64,
     profit_mil: u64,
     asset_mil: u64,
@@ -24,6 +27,11 @@ struct Company {
 #[derive(Debug)]
 struct AppState {
     selected_company: RwLock<Option<Company>>,
+    total_games: RwLock<u32>,
+    correct_guesses: RwLock<u32>,
+    incorrect_guesses: RwLock<u32>,
+    total_time: RwLock<u32>, // Add total_time field
+
 }
 
 #[derive(FromForm)]
@@ -31,12 +39,28 @@ struct Guess {
     guess_type: String,
 }
 
+#[derive(Deserialize)]
+struct TimePayload {
+    time: u32,
+}
+
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Stats {
+    total_games: u32,
+    correct_guesses: u32,
+    incorrect_guesses: u32,
+    total_time: u32,
+}
+
 fn read_csv() -> Result<Vec<Company>, std::io::Error> {
-    let file = File::open("data/f500data.csv")?;
+    let file = File::open("data/f500data-shortlist.csv")?;
     let mut rdr = csv::Reader::from_reader(file);
     let mut companies = Vec::new();
+
     for result in rdr.deserialize() {
         let record: Company = result?;
+        // println!("Read company: {:?}", record.rank); // Debug output
         companies.push(record);
     }
     Ok(companies)
@@ -58,109 +82,199 @@ fn company(state: &State<AppState>) -> Result<Json<Company>, &'static str> {
     Ok(Json(company.clone()))
 }
 
+fn format_billion(value: u64) -> String {
+    format!("${:.1}B", value as f64 / 1_000.0)
+}
+
 #[post("/submit_guess", data = "<guess>")]
 fn submit_guess(guess: Form<Guess>, state: &State<AppState>) -> String {
     let guess = guess.into_inner();
-    let market_cap_estimate = 50_000; // value in millions
-    let revenue_estimate = 30_000; // value in millions
-    let profit_estimate = 10_000; // value in millions
-    let assets_estimate = 25_000; // value in millions
-    let employees_estimate = 30_000; // raw value
 
     let selected_company = state.selected_company.read().unwrap();
+
     if let Some(ref company) = *selected_company {
-        let formatted_market_cap_billion = format!("${:.1}B", company.market_value_mil as f64 / 1_000.0);
-        let formatted_revenue_billion = format!("${:.1}B", company.revenue_mil as f64 / 1_000.0);
-        let formatted_profit_billion = format!("${:.1}B", company.profit_mil as f64 / 1_000.0);
+        let market_cap_estimate = if company.rank <= 250 { 40_000 } else { 10_000 }; // value in millions
+        let revenue_estimate = if company.rank <= 250 { 30_000 } else { 7_500 }; // value in millions
+        let profit_estimate = if company.rank <= 250 { 10_000 } else { 2_500 }; // value in millions
+        let assets_estimate = if company.rank <= 250 { 25_000 } else { 6_000 }; // value in millions
+        let employees_estimate = if company.rank <= 250 { 30_000 } else { 7_500 }; // raw value
+
+        let formatted_market_cap_billion = format_billion(company.marketcap);
+        let formatted_revenue_billion = format_billion(company.revenue_mil);
+        let formatted_profit_billion = format_billion(company.profit_mil);
         let formatted_assets_billion = format!("${:.1}B", company.asset_mil as f64 / 1_000.0);
-        let formatted_employees = format!("{}", company.employees);
+        let formatted_employees = format!("{}", company.employees.to_formatted_string(&Locale::en));
+
+        let mut correct_guesses = state.correct_guesses.write().unwrap();
+        let mut incorrect_guesses = state.incorrect_guesses.write().unwrap();
+        let mut total_games = state.total_games.write().unwrap();
 
         let result = match guess.guess_type.as_str() {
             "market_cap_higher" => {
-                if company.market_value_mil > market_cap_estimate {
-                    format!("Correct! The actual market cap of {} is {} which is higher than $40.0B", company.name, formatted_market_cap_billion)
+                if company.marketcap > market_cap_estimate {
+                    *correct_guesses += 1;
+                    format!("Correct! The actual market cap of {} is {} which is higher than ${:.1}B", company.name, formatted_market_cap_billion, market_cap_estimate as f64 / 1_000.0)
                 } else {
-                    format!("Incorrect! The actual market cap of {} is {} which is lower than $40.0B", company.name, formatted_market_cap_billion)
+                    *incorrect_guesses += 1;
+                    format!("Incorrect! The actual market cap of {} is {} which is lower than ${:.1}B", company.name, formatted_market_cap_billion, market_cap_estimate as f64 / 1_000.0)
                 }
             }
             "market_cap_lower" => {
-                if company.market_value_mil < market_cap_estimate {
-                    format!("Correct! The actual market cap of {} is {} which is lower than $40.0B", company.name, formatted_market_cap_billion)
+                if company.marketcap < market_cap_estimate {
+                    *correct_guesses += 1;
+                    format!("Correct! The actual market cap of {} is {} which is lower than ${:.1}B", company.name, formatted_market_cap_billion, market_cap_estimate as f64 / 1_000.0)
                 } else {
-                    format!("Incorrect! The actual market cap of {} is {} which is higher than $40.0B", company.name, formatted_market_cap_billion)
+                    *incorrect_guesses += 1;
+                    format!("Incorrect! The actual market cap of {} is {} which is higher than ${:.1}B", company.name, formatted_market_cap_billion, market_cap_estimate as f64 / 1_000.0)
                 }
             }
             "revenue_higher" => {
                 if company.revenue_mil > revenue_estimate {
-                    format!("Correct! The actual revenue of {} is {} which is higher than $30.0B", company.name, formatted_revenue_billion)
+                    *correct_guesses += 1;
+                    format!("Correct! The actual revenue of {} is {} which is higher than ${:.1}B", company.name, formatted_revenue_billion, revenue_estimate as f64 / 1_000.0)
                 } else {
-                    format!("Incorrect! The actual revenue of {} is {} which is lower than $30.0B", company.name, formatted_revenue_billion)
+                    *incorrect_guesses += 1;
+
+                    format!("Incorrect! The actual revenue of {} is {} which is lower than ${:.1}B", company.name, formatted_revenue_billion, revenue_estimate as f64 / 1_000.0)
                 }
             }
             "revenue_lower" => {
                 if company.revenue_mil < revenue_estimate {
-                    format!("Correct! The actual revenue of {} is {} which is lower than $30.0B", company.name, formatted_revenue_billion)
+                    *correct_guesses += 1;
+                    format!("Correct! The actual revenue of {} is {} which is lower than ${:.1}B", company.name, formatted_revenue_billion, revenue_estimate as f64 / 1_000.0)
                 } else {
-                    format!("Incorrect! The actual revenue of {} is {} which is higher than $30.0B", company.name, formatted_revenue_billion)
+                    *incorrect_guesses += 1;
+                    format!("Incorrect! The actual revenue of {} is {} which is higher than ${:.1}B", company.name, formatted_revenue_billion, revenue_estimate as f64 / 1_000.0)
                 }
             }
 
             "profit_higher" => {
                 if company.profit_mil > profit_estimate {
-                    format!("Correct! The actual profit of {} is {} which is higher than $10.0B", company.name, formatted_profit_billion)
+                    *correct_guesses += 1;
+                    println!("correct guess + 1");
+                    format!("Correct! The actual profit of {} is {} which is higher than ${:.1}B", company.name, formatted_profit_billion, profit_estimate as f64 / 1_000.0)
                 } else {
-                    format!("Incorrect! The actual profit of {} is {} which is lower than $10.0B", company.name, formatted_profit_billion)
+                    *incorrect_guesses += 1;
+
+                    format!("Incorrect! The actual profit of {} is {} which is lower than ${:.1}B", company.name, formatted_profit_billion, profit_estimate as f64 / 1_000.0)
                 }
             }
             "profit_lower" => {
                 if company.profit_mil < profit_estimate {
-                    format!("Correct! The actual profit of {} is {} which is lower than $10.0B", company.name, formatted_profit_billion)
+                    *correct_guesses += 1;
+                    println!("correct guess + 1");
+                    format!("Correct! The actual profit of {} is {} which is lower than ${:.1}B", company.name, formatted_profit_billion, profit_estimate as f64 / 1_000.0)
                 } else {
-                    format!("Incorrect! The actual profit of {} is {} which is higher than $10.0B", company.name, formatted_profit_billion)
+                    *incorrect_guesses += 1;
+
+                    format!("Incorrect! The actual profit of {} is {} which is higher than ${:.1}B", company.name, formatted_profit_billion, profit_estimate as f64 / 1_000.0)
                 }
             }
             "assets_higher" => {
                 if company.asset_mil > assets_estimate {
-                    format!("Correct! The actual assets of {} are {} which is higher than $25.0B", company.name, formatted_assets_billion)
+                    *correct_guesses += 1;
+                    println!("correct guess + 1");
+                    format!("Correct! The actual assets of {} are {} which is higher than ${:.1}B", company.name, formatted_assets_billion, assets_estimate as f64 / 1_000.0)
                 } else {
-                    format!("Incorrect! The actual assets of {} are {} which is lower than $25.0B", company.name, formatted_assets_billion)
+                    *incorrect_guesses += 1;
+
+                    format!("Incorrect! The actual assets of {} are {} which is lower than ${:.1}B", company.name, formatted_assets_billion, assets_estimate as f64 / 1_000.0)
                 }
             }
             "assets_lower" => {
                 if company.asset_mil < assets_estimate {
-                    format!("Correct! The actual assets of {} are {} which is lower than $25.0B", company.name, formatted_assets_billion)
+                    *correct_guesses += 1;
+                    println!("correct guess + 1");
+                    format!("Correct! The actual assets of {} are {} which is lower than ${:.1}B", company.name, formatted_assets_billion, assets_estimate as f64 / 1_000.0)
                 } else {
-                    format!("Incorrect! The actual assets of {} are {} which is higher than $25.0B", company.name, formatted_assets_billion)
+                    *incorrect_guesses += 1;
+                    format!("Incorrect! The actual assets of {} are {} which is higher than ${:.1}B", company.name, formatted_assets_billion, assets_estimate as f64 / 1_000.0)
                 }
             }
             "employees_higher" => {
                 if company.employees > employees_estimate {
-                    format!("Correct! The actual employees of {} are {} which is higher than 30,000", company.name, formatted_employees)
+                    *correct_guesses += 1;
+                    println!("correct guess + 1");
+                    format!("Correct! The actual employees of {} is {} which is higher than {}", company.name, formatted_employees, employees_estimate.to_formatted_string(&Locale::en))
                 } else {
-                    format!("Incorrect! The actual employees of {} are {} which is lower than 30,000", company.name, formatted_employees)
+                    *incorrect_guesses += 1;
+                    format!("Incorrect! The actual employees of {} is {} which is lower than {}", company.name, formatted_employees, employees_estimate.to_formatted_string(&Locale::en))
                 }
             }
             "employees_lower" => {
                 if company.employees < employees_estimate {
-                    format!("Correct! The actual employees of {} are {} which is lower than 30,000", company.name, formatted_employees)
+                    *correct_guesses += 1;
+                    println!("correct guess + 1");
+                    format!("Correct! The actual employees of {} is {} which is lower than {}", company.name, formatted_employees, employees_estimate.to_formatted_string(&Locale::en))
                 } else {
-                    format!("Incorrect! The actual employees of {} are {} which is higher than 30,000", company.name, formatted_employees)
+                    *incorrect_guesses += 1;
+                    format!("Incorrect! The actual employees of {} is {} which is higher than {}", company.name, formatted_employees, employees_estimate.to_formatted_string(&Locale::en))
                 }
             }
             _ => "Invalid guess".to_string(),
         };
+        if (*correct_guesses + *incorrect_guesses) % 5 == 0 {
+            *total_games += 1;
+        }
+
         return result;
     }
     "Failed to process the guess".to_string()
 }
 
+#[get("/stats")]
+fn get_stats(state: &State<AppState>) -> Result<Json<Stats>, &'static str> {
+    let total_games = *state.total_games.read().unwrap();
+    let correct_guesses = *state.correct_guesses.read().unwrap();
+    let incorrect_guesses = *state.incorrect_guesses.read().unwrap();
+    let total_time = *state.total_time.read().unwrap();
+
+    let stats = Stats {
+        total_games,
+        correct_guesses,
+        incorrect_guesses,
+        total_time
+    };
+
+    Ok(Json(stats))
+}
+
+#[post("/stats", data = "<payload>")]
+fn update_stats(state: &State<AppState>, payload: Json<TimePayload>) -> Result<Json<Stats>, &'static str> {
+    {
+        let mut total_time = state.total_time.write().unwrap();
+        *total_time += payload.time;
+    }
+
+    let total_time = *state.total_time.read().unwrap();
+    let total_games = *state.total_games.read().unwrap();
+    let incorrect_guesses = *state.incorrect_guesses.read().unwrap();
+    let correct_guesses = *state.correct_guesses.read().unwrap();
+
+    let stats = Stats {
+        total_games,
+        correct_guesses,
+        incorrect_guesses,
+        total_time
+    };
+    
+    Ok(Json(stats))
+}
+
+
+
+
 #[launch]
 fn rocket() -> Rocket<Build> {
     let state = AppState {
         selected_company: RwLock::new(None),
+        total_games: RwLock::new(0),
+        correct_guesses: RwLock::new(0),
+        incorrect_guesses: RwLock::new(0),
+        total_time: RwLock::new(0),
     };
     rocket::build()
         .manage(state)
-        .mount("/", routes![company, submit_guess])
+        .mount("/", routes![company, submit_guess, get_stats, update_stats])
         .mount("/", rocket::fs::FileServer::from(rocket::fs::relative!("static")))
 }
--- static/index.js
+++ static/index.js
@@ -7,6 +7,20 @@ document.addEventListener('DOMContentLoaded', function() {
         employees: null
     }
 
+    // let total_time = 0;
+    let timer = 0;
+    let timerInterval;
+    const timerElement = document.getElementById('timer');
+
+    function startTimer() {
+        timerInterval = setInterval(() => {
+            timer++;
+            timerElement.textContent = `Time spent: ${timer}s`;
+        }, 1000);
+    }
+
+    startTimer();
+
     fetch('/company')
         .then(response => response.json())
         .then(data => {
@@ -16,11 +30,12 @@ document.addEventListener('DOMContentLoaded', function() {
             const companyDescription = document.getElementById('company-description');
             companyDescription.textContent = `Description: ${data.description}\n\n`;
 
-            document.getElementById('market-cap-question').textContent = "Is the market cap lower or higher than $40.0B?";
-            document.getElementById('revenue-question').textContent = "Is the revenue lower or higher than $30.0B?";
-            document.getElementById('profit-question').textContent = "Is the profit lower or higher than $10.0B?";
-            document.getElementById('assets-question').textContent = "Is the value of assets higher or lower than $25.0B?";
-            document.getElementById('employees-question').textContent = "Is the number of employees higher or lower than 30,000?";
+            document.getElementById('market-cap-question').textContent = `Is the market cap lower or higher than ${data.rank <= 250 ? "$40.0B" : "$10.0B"}?`;
+            // console.log(data.rank);
+            document.getElementById('revenue-question').textContent = `Is the revenue lower or higher than ${data.rank <= 250 ? "$30.0B" : "$7.5B"}?`;
+            document.getElementById('profit-question').textContent = `Is the profit lower or higher than ${data.rank <= 250 ? "$10.0B" : "$2.5B"}?`;
+            document.getElementById('assets-question').textContent = `Is the value of assets higher or lower than ${data.rank <= 250 ? "$25.0B" : "$6.0B"}?`;
+            document.getElementById('employees-question').textContent = `Is the number of employees higher or lower than ${data.rank <= 250 ? "30,000" : "7,500"}?`;
         })
         .catch(error => console.error('Error fetching company:', error));
 
@@ -56,10 +71,11 @@ document.addEventListener('DOMContentLoaded', function() {
         submitGuess('employees_lower', 'response-employees', ['employees-higher', 'employees-lower'], 'employees');
     });
 
-       document.getElementById('play-again').addEventListener('click', function() {
+    document.getElementById('play-again').addEventListener('click', function() {
            location.reload();
        });
-       function submitGuess(guess, responseElementId, buttonIdsToDisable, scoreKey){
+
+    function submitGuess(guess, responseElementId, buttonIdsToDisable, scoreKey){
         fetch('/submit_guess', {
             method: 'POST',
             headers: {
@@ -71,7 +87,10 @@ document.addEventListener('DOMContentLoaded', function() {
         .then(data => {
             const responseElement = document.getElementById(responseElementId);
             responseElement.textContent = data;
-            responseElement.classList.remove('hidden');
+
+            // Remove any existing class
+            responseElement.classList.remove('correct', 'incorrect');
+            responseElement.classList.add(data.includes('Correct!') ? 'correct' : 'incorrect');
     
             buttonIdsToDisable.forEach(buttonId => {
                 document.getElementById(buttonId).disabled = true;
@@ -81,6 +100,23 @@ document.addEventListener('DOMContentLoaded', function() {
     
             if (score.market_cap !== null && score.revenue !== null && score.profit !== null && score.assets !== null && score.employees !== null) {
                 displayOverallScore()
+                fetch('/stats', {
+                    method: 'POST',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify({ time: timer })
+                })
+                .then(response => response.json())
+                .then(data => {
+                    console.log(data);
+                    const statsElement = document.getElementById('overall-stats');
+                    let accuracy = (data.correct_guesses / (data.total_games * 5) * 100).toFixed(2);
+                    statsElement.textContent = `Overall accuracy: ${accuracy}% Games Played: ${data.total_games}, Correct Guesses: ${data.correct_guesses}, Incorrect Guesses: ${data.incorrect_guesses}`;
+                    statsElement.classList.remove('hidden');
+                    timerElement.textContent = `Time: ${timer}s. \n Average time per round: ${(data.total_time / data.total_games).toFixed(2)}s`;
+                    clearInterval(timerInterval);
+                })
+                .catch(error => console.error('Error fetching stats:', error));
+
             }
         })
         .catch(error => {
@@ -88,10 +124,16 @@ document.addEventListener('DOMContentLoaded', function() {
         })
     }
     function displayOverallScore() {
+        document.getElementById('response-market-cap').classList.remove('hidden');
+        document.getElementById('response-revenue').classList.remove('hidden');
+        document.getElementById('response-profit').classList.remove('hidden');
+        document.getElementById('response-assets').classList.remove('hidden');
+        document.getElementById('response-employees').classList.remove('hidden');
+
         const overallScore = Object.values(score).filter(value => value === 'Correct!').length;
         const percentage = overallScore / 5 * 100;  
         const overallScoreElement = document.getElementById('overall-score');
-    
+
         overallScoreElement.textContent = `Your scored: ${percentage}% Share your score on X (formerly Twitter)!`;
         overallScoreElement.classList.remove('hidden');
     }
